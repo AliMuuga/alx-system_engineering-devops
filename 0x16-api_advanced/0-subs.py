0e8f0eb1c0dd266e91b72c3c0215aa7bd77a30ad
AliMuuga <alithareaper@gmail.com>
2024-08-14 01:46:23 -0700

    Hello

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""Query Reddit API to get the number of subscribers for a given subreddit"""
-
+""" Exporting csv files"""
+import json
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
-    """Fetches the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The subreddit name.
-
-    Returns:
-        int: The number of subscribers if the subreddit is valid, otherwise 0.
-    """
-    headers = {'User-Agent': 'my_reddit_app/0.1'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
-
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
+    else:
+        return(0)
